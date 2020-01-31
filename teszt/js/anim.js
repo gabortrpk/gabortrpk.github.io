751fc1b37f25c726e6c8f951080f31eee5539185
gabortrpk <40609873+gabortrpk@users.noreply.github.com>
2020-01-31 20:25:00 +0100

    Update anim.js

--- teszt/js/anim.js
+++ teszt/js/anim.js
@@ -1,6 +1,6 @@
 let animationOne = lottie.loadAnimation({ 
 	container: document.getElementById('animation'), // Required
-	path: 'data.json', // Required
+	path: './js/data.json', // Required
 	renderer: 'svg', // Required
 	loop: true, // Optional
 	autoplay: true, // Optional,
